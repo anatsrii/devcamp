3db01dfec38bcbdd70e7935ba65d8257bb987391
anatsrii <anat.allsolution@gmail.com>
2022-06-28 22:49:31 +0700

    Try to fetch, axios OK homework2 done

--- Day14-NodeJS-Express/homework2/front/src/App.js
+++ Day14-NodeJS-Express/homework2/front/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import { Breadcrumb, Layout, } from 'antd';
+import { Breadcrumb, Card, Col, Layout, Row, } from 'antd';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Meta from 'antd/lib/card/Meta';
 const { Content } = Layout;
 
 function App() {
@@ -9,14 +10,13 @@ function App() {
   
   useEffect( ()=> {
     targetUser();
-  }, [])
+  }, []);
+
   const targetUser = async ()=> {
     const userData = await axios.get('/api/user');
     setUser(userData.data.test);
   }
-const mapUser = user.map((obj)=> {
-  return <div>{obj.email}</div>
-})
+
 
   return (
     <Layout className="layout">
@@ -31,8 +31,25 @@ const mapUser = user.map((obj)=> {
           margin: '16px 0',
         }}
         >
-          {mapUser}
+
       </Breadcrumb>
+        {user.map((obj, idx)=> {
+          return(
+            <Row gutter={{lg: 32,}} style={{alignItems: "center", justifyContent: "center"}}>
+              <Col span={16}  >
+                <Card key={idx}
+                hoverable 
+                style={{width: 160, alignItems: "center", justifyContent: "center"}}
+                cover={<img alt='avatar' src={`./${obj.avatar}`}/>}
+                >
+                <Meta description={`${obj.first_name} ${'\u00A0'} ${obj.last_name}`}/>
+                <Meta description={obj.email}/>
+
+                </Card>
+              </Col>
+            </Row>
+          )
+        })}
 
     </Content>
   
